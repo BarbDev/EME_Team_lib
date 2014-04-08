40b8d371e17e15e7cad47d429d21849a96637165
Tanguy <BarbDevelop@gmail.com>
2014-04-08 18:04:05 +0200

    -mise à jour du setup(); maintenant le programme ne rentre pas dans la boucle principale tant qu'il n'a pas détecté de manette (évite de reset plusieurs fois avant d'avoir la détection) -ajouts de 2 modes de commandes des roues du robot en fonction du temps écoulé

--- EME_Team_lib/Examples/PS2X_Golgoth_master/PS2X_Golgoth_master.ino
+++ EME_Team_lib/Examples/PS2X_Golgoth_master/PS2X_Golgoth_master.ino
@@ -31,12 +31,14 @@ byte type = 0; // variable stockant le type de contrôleur, voir plus bas pour s
 void setup() { 
     Serial.begin(57600); // définition de la vitesse de transmission pour pouvoir communiquer avec le PC et avoir un retour sur la console
 
-    // définition des pins et paramètre et vérification des erreurs: GamePad(clock, command, attention, data, Pressures?, Rumble?)
-    error = manette.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
-    manette.printCheckError(error); // Permet le retour d'éventuelles erreurs dans la console arduino
-
-    type = manette.readType(); 
-    manette.printCheckControllerType(type); // Permet d'avoir le retour du type de controller dans la console arduino
+    // Cherche à synchroniser jusqu'à ce que la carte arduino est établi la communication
+    do {
+        // définition des pins et paramètre et vérification des erreurs: GamePad(clock, command, attention, data, Pressures?, Rumble?)
+        error = manette.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
+        manette.printCheckError(error); // Permet le retour d'éventuelles erreurs dans la console arduino
+        type = manette.readType(); 
+        manette.printCheckControllerType(type); // Permet d'avoir le retour du type de controller dans la console arduino
+    } while (error != 0 && type == 0);
 
     SPI.begin();
 }
@@ -59,27 +61,7 @@ void loop() {
     else if (type == 1 || type == 3) { //DualShock Controller (avec ou sans fil trouvée)
         manette.read_gamepad(); // lit les données de la manette
 
-        if (manette.Analog(PSS_RY) == 255) {
-            moteurDroit.incrementSpeed(5);
-            moteurDroit.setDir(CLOCKWISE);
-        }
-        else if (manette.Analog(PSS_RY) == 0) {
-            moteurDroit.incrementSpeed(5);
-            moteurDroit.setDir(ANTI_CLOCKWISE);
-        }
-        else
-            moteurDroit.stop();
-
-        if (manette.Analog(PSS_LY) == 255) {
-            moteurGauche.incrementSpeed(5);
-            moteurGauche.setDir(CLOCKWISE);
-        }
-        else if (manette.Analog(PSS_LY) == 0) {
-            moteurGauche.incrementSpeed(5);
-            moteurGauche.setDir(ANTI_CLOCKWISE);
-        }
-        else
-            moteurGauche.stop();
+        tank_control_OT();
 
         if (manette.Button(PSB_L1))
             envoyer(SERVO_1, 0)
@@ -96,4 +78,63 @@ void loop() {
 
         delay(50);  // attend 50ms~
     }
+}
+
+void tank_control_OT() {
+    /* Roue droite */
+    if (manette.Analog(PSS_RY) == 255) {
+        moteurDroit.incrementSpeed(5);
+        moteurDroit.setDir(CLOCKWISE);
+    }
+    else if (manette.Analog(PSS_RY) == 0) {
+        moteurDroit.incrementSpeed(5);
+        moteurDroit.setDir(ANTI_CLOCKWISE);
+    }
+    else
+        moteurDroit.stop();
+
+    /* Roue gauche */
+    if (manette.Analog(PSS_LY) == 255) {
+        moteurGauche.incrementSpeed(5);
+        moteurGauche.setDir(CLOCKWISE);
+    }
+    else if (manette.Analog(PSS_LY) == 0) {
+        moteurGauche.incrementSpeed(5);
+        moteurGauche.setDir(ANTI_CLOCKWISE);
+    }
+    else
+        moteurGauche.stop();
+
+    /* Mise à jour des moteurs */
+    moteurGauche.update();
+    moteurDroit.update();
+}
+
+void regular_control_OT() {
+    /* Contrôle de la vitesse de déplacement avec le joystick droit*/
+    if (manette.Analog(PSS_RY) == 255) {
+        moteurDroit.incrementSpeed(5);
+        moteurDroit.setDir(CLOCKWISE);
+        moteurGauche.incrementSpeed(5);
+        moteurGauche.setDir(CLOCKWISE);
+    }
+    else if (manette.Analog(PSS_RY) == 0) {
+        moteurDroit.incrementSpeed(5);
+        moteurDroit.setDir(ANTI_CLOCKWISE);
+        moteurGauche.incrementSpeed(5);
+        moteurGauche.setDir(ANTI_CLOCKWISE);
+    }
+    else {
+        moteurDroit.stop();
+        moteurGauche.stop();
+    }
+
+    /* Contrôle de la direction avec le joystick gauche */
+    if (manette.Analog(PSS_LX) == 0) // On veut tourner à gauche
+        moteurGauche.stop();
+    else if (manette.Analog(PSS_LX) == 255) // On veut tourner à droite
+        moteurDroit.stop();
+        
+    moteurGauche.update();
+    moteurDroit.update();
 }
