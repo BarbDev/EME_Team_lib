5841949e5f261bbca27055f9a12c1113b25f7a71
Tanguy <BarbDevelop@gmail.com>
2014-04-30 18:06:07 +0200

    -ajout du control d'un 3ème moteur -ajout des contrôles pour 4servomoteurs

--- EME_Team_lib/Examples/PS2X_Golgoth/PS2X_Golgoth.ino
+++ EME_Team_lib/Examples/PS2X_Golgoth/PS2X_Golgoth.ino
@@ -12,19 +12,14 @@ PS2X manette; // création d'une classe de type manette
 
 Motor moteurGauche(PWM_PIN_MOTOR_1, DIR_PIN_MOTOR_1);
 Motor moteurDroit(PWM_PIN_MOTOR_2, DIR_PIN_MOTOR_2);
+Motor moteur3(PWM_PIN_MOTOR_3, DIR_PIN_MOTOR_3);
 Relais relais1(PIN_RELAIS_1), relais2(PIN_RELAIS_2);
-Servo servo1, servo2;
-
-////////////////////////////////////////////////////////////
-// ATTENTION:
-// pour le moment la bibliothèque ne supporte la connection des
-// contrôleurs à chaud. Il faut donc soit redémarrer la carte
-// arduino après avoir connecté le contrôleur, soit faire de
-// nouveau appel à la fonction config_gamepad(...)
-////////////////////////////////////////////////////////////
+Servo servo1, servo2, servo3, servo4;
 
 int error = 0; // variable stockant le code d'erreur, voir plus bas pour savoir à quelle erreur correspond tel code
 byte type = 0; // variable stockant le type de contrôleur, voir plus bas pour savoir les types possibles
+byte acceleration = 8; // variable stockant l'accélération des moteurs contrôlant les roues du robot
+byte angleServo3 = 0, angleServo4 = 0;
 
 ////////////////////////////////////////////////////////////
 // fonction obligatoire d'arduino, elle est n'est effectué qu'une
@@ -44,6 +39,8 @@ void setup() {
 
     servo1.attach(PIN_SERVO_1, 0, 179);
     servo2.attach(PIN_SERVO_2, 0, 179);
+    servo3.attach(PIN_SERVO_3, 0, 179);
+    servo4.attach(PIN_SERVO_4, 0, 179);
 }
 
 
@@ -64,25 +61,66 @@ void loop() {
     else if (type == 1 || type == 3) { //DualShock Controller (avec ou sans fil trouvée)
         manette.read_gamepad(); // lit les données de la manette
 
-        regular_control_OT();
+        tank_control_OT();
 
+        /* Contrôle du 3eme moteur */
+        if (manette.Button(PSB_CROSS)) {
+            moteur3.setSpeed(255);
+            moteur3.setDir(CLOCKWISE);
+        }
+        else if (manette.Button(PSB_SQUARE)) {
+            moteur3.setSpeed(255);
+            moteur3.setDir(ANTI_CLOCKWISE);
+        }
+        else
+            moteur3.stop();
+
+        /* Contrôle des servos*/
+        /* Servo 1 */
         if (manette.Button(PSB_L1))
             servo1.write(0);
         else if (manette.Button(PSB_L2))
             servo1.write(179);
-
+        /* Servo 2 */
         if (manette.Button(PSB_R1))
             servo2.write(0);
         else if (manette.Button(PSB_R2))
             servo2.write(179);
+        /* Servo 3 */
+        if (manette.Button(PSB_PAD_UP)) {
+            if (angleServo3 + 1 < 180)
+                angleServo3++;
+            servo3.write(angleServo3);
+        }
+        else if (manette.Button(PSB_PAD_DOWN)) {
+            if (angleServo3 - 1 >= 0)
+                angleServo3--;
+            servo3.write(angleServo3);
+        }
+        /* Servo 4 */
+        if (manette.Button(PSB_PAD_RIGHT)) {
+            if (angleServo4 + 1 < 180)
+                angleServo4++;
+            servo4.write(angleServo4);
+        }
+        else if (manette.Button(PSB_PAD_LEFT)) {
+            if (angleServo4 - 1 >= 0)
+                angleServo4--;
+            servo4.write(angleServo4);
+        }
 
+        /* Contrôle des relais*/
         if (manette.Button(PSB_TRIANGLE))
             relais1.changeState();
         if (manette.Button(PSB_CIRCLE))
             relais2.changeState();
 
+        /* Mise à jour des relais */
         relais1.update();
         relais2.update();
+
+        /* Mise à jour du moteur 3*/
+        moteur3.update();
 
         delay(50);  // attend 50ms~
     }
@@ -91,11 +129,11 @@ void loop() {
 void tank_control_OT() {
     /* Roue droite */
     if (manette.Analog(PSS_RY) == 255) {
-        moteurDroit.incrementSpeed(5);
+        moteurDroit.incrementSpeed(acceleration);
         moteurDroit.setDir(CLOCKWISE);
     }
     else if (manette.Analog(PSS_RY) == 0) {
-        moteurDroit.incrementSpeed(5);
+        moteurDroit.incrementSpeed(acceleration);
         moteurDroit.setDir(ANTI_CLOCKWISE);
     }
     else
@@ -103,11 +141,11 @@ void tank_control_OT() {
 
     /* Roue gauche */
     if (manette.Analog(PSS_LY) == 255) {
-        moteurGauche.incrementSpeed(5);
+        moteurGauche.incrementSpeed(acceleration);
         moteurGauche.setDir(CLOCKWISE);
     }
     else if (manette.Analog(PSS_LY) == 0) {
-        moteurGauche.incrementSpeed(5);
+        moteurGauche.incrementSpeed(acceleration);
         moteurGauche.setDir(ANTI_CLOCKWISE);
     }
     else
@@ -121,15 +159,15 @@ void tank_control_OT() {
 void regular_control_OT() {
     /* Contrôle de la vitesse de déplacement avec le joystick droit*/
     if (manette.Analog(PSS_RY) == 255) {
-        moteurDroit.incrementSpeed(5);
+        moteurDroit.incrementSpeed(acceleration);
         moteurDroit.setDir(CLOCKWISE);
-        moteurGauche.incrementSpeed(5);
+        moteurGauche.incrementSpeed(acceleration);
         moteurGauche.setDir(CLOCKWISE);
     }
     else if (manette.Analog(PSS_RY) == 0) {
-        moteurDroit.incrementSpeed(5);
+        moteurDroit.incrementSpeed(acceleration);
         moteurDroit.setDir(ANTI_CLOCKWISE);
-        moteurGauche.incrementSpeed(5);
+        moteurGauche.incrementSpeed(acceleration);
         moteurGauche.setDir(ANTI_CLOCKWISE);
     }
     else {
