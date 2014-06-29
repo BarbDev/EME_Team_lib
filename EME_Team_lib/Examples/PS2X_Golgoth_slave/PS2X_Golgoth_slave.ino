266e5d2bc0ad12532a3169a096b0d7836b6926fe
BarbDev <BarbDevelop@gmail.com>
2014-06-29 23:00:05 +0200

    -suppression du define SLAVE désormais inutile -inclusion du bon fichier communication -simplification du procédé de mise à jour des relais (ces derniers par contre se mettent à jour ttes les 5µs)

--- EME_Team_lib/Examples/PS2X_Golgoth_slave/PS2X_Golgoth_slave.ino
+++ EME_Team_lib/Examples/PS2X_Golgoth_slave/PS2X_Golgoth_slave.ino
@@ -7,15 +7,11 @@
 #include <Relais.h>
 
 #include "constantes.h"
-#define SLAVE // voir l'effet dans communication.h
-#include "communication.h"
+#include "communicationS.h"
 
 Servo servo1, servo2, servo3;
 Relais relais1(PIN_RELAIS_1), relais2(PIN_RELAIS_2), relais3(PIN_RELAIS_3), relais4(PIN_RELAIS_4);
-//extern volatile boolean process_it;
-//extern volatile byte buffer[2];
-extern volatile byte IDs[7];
-//int i = 0;
+extern volatile byte IDs[7]; // Définit dans le fichier communicationS.h
 
 ////////////////////////////////////////////////////////////
 // fonction obligatoire d'arduino, elle est n'est effectué qu'une
@@ -25,23 +21,21 @@ extern volatile byte IDs[7];
 void setup() { 
     Serial.begin(57600); // définition de la vitesse de transmission pour pouvoir communiquer avec le PC et avoir un retour sur la console
 
+    /* Attachement des servos à leurs pins correspondantes et définition de l'angle minimal et maximal */
     servo1.attach(PIN_SERVO_1, 0, 179);
     servo2.attach(PIN_SERVO_2, 0, 179);
     servo3.attach(PIN_SERVO_3, 0, 179);
 
     initInterrupt()
-
-    Serial.println("Initialized");
 }
 
-
+////////////////////////////////////////////////////////////
+// Le programme va constamment mettre à jour les composants
+// toutes les 5µs.
+// Il faut trouver un moyen de soulager le microcontroleur
+// en supprimant toutes les mises à jours inutiles.
+////////////////////////////////////////////////////////////
 void loop() {
-    // voir communication.h
-    /*if (process_it) {
-        Serial.println("Processing");
-    	recevoir()
-    	process_it = false;
-    }*/
     servo1.write(IDs[SERVO_1 - 181]);
     servo2.write(IDs[SERVO_2 - 181]);
     servo3.write(IDs[SERVO_3 - 181]);
@@ -53,23 +47,6 @@ void loop() {
     relais3.update();
     relais4.setState(IDs[RELAIS_4 - 181]);
     relais4.update();
-    /*for (i = 0; i < 7; i++) {
-        Serial.print("Tab[");
-        Serial.print(i, DEC);
-        Serial.print("] = ");
-        Serial.println(IDs[i], DEC);
-    }*/
-
-    /*
-    relais1.setState(HIGH);
-    relais1.update();
-    relais2.setState(HIGH);
-    relais2.update();
-
-    relais3.setState(HIGH);
-    relais3.update();
-    relais4.setState(HIGH);
-    relais4.update();*/
 
     delayMicroseconds(5);
 }
